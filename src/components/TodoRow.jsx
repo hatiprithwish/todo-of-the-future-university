571b1bd1a025a130739d17ff22b1e173f3b6cd6c
Prithwish Hati <hatiprithwish@gmail.com>
2024-05-15 15:07:18 +0530

    bug fix: deleting duplicate todos

--- src/components/TodoRow.jsx
+++ src/components/TodoRow.jsx
@@ -6,7 +6,9 @@ const TodoRow = ({ todo, id, todos, setTodos }) => {
   const [isHoverd, setIsHovered] = useState(false);
 
   const handleDelete = (TodoId) => {
-    const updatedTodos = todos.filter((item, i) => item[i] !== item[TodoId]);
+    const updatedTodos = todos.filter((item, i) => {
+      if (i !== TodoId) return item;
+    });
     setTodos(updatedTodos);
   };
 
